9d2f6dfa2ef12a83077d0b1082abc99ca5cf0ea6
garrettstott <garrettstott@gmail.com>
2017-01-16 11:59:24 -0700

    the-shop height

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -6,13 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
   height = windowHeight + 'px'
 
   home = document.getElementById('home')
-  theShop = document.getElementById('the-shop')
   services = document.getElementById('services')
   clippers = document.getElementById('clippers')
 
-  console.log(height)
   home.style.height = height
-  theShop.style.height = height
   services.style.height = height
   clippers.style.height = (windowHeight * 0.6) + 'px'
 
