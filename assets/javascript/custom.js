e47e0cd138aa4c5ca4b84a6281a3f177d8da2add
garrettstott <garrettstott@gmail.com>
2017-01-16 10:37:49 -0700

    height on mobile

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
   var year = new Date().getFullYear()
   document.getElementById('current-year').innerHTML = year
+
+  windowHeight = window.innerHeight
+  height = windowHeight + 'px'
+
+  home = document.getElementById('home')
+  theShop = document.getElementById('the-shop')
+  services = document.getElementById('services')
+  clippers = document.getElementById('clippers')
+
+  console.log(height)
+  home.style.height = height
+  theShop.style.height = height
+  services.style.height = height
+  clippers.style.height = (windowHeight * 0.6) + 'px'
+
 });
